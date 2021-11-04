e917a38637b85c11756818877756910693edf2aa
Dalton Bunker <dbbunker3@gmail.com>
2021-11-04 00:07:36 -0600

    all buttons work

--- axios_todo/main.js
+++ axios_todo/main.js
@@ -1,32 +1,56 @@
 todoUrl = "https://api.vschool.io/dalton/todo/";
-const addBtn = document.querySelector("#add-btn");
+let currentId = "";
+const addNewBtn = document.querySelector("#add-btn");
+const submitBtn = document.querySelector("#submit-btn");
 const todoContainer = document.querySelector(".todo-container");
 const todoForm = document["todo-form"];
 
-addBtn.addEventListener("click", addBtnClicked);
+addNewBtn.addEventListener("click", displayForm);
 todoForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    onFormSubmit();
+    const buttonText = e.submitter.textContent;
+    if (buttonText === "ADD") {
+        onFormSubmit();
+    } else {
+        onFormSave();
+    }
 })
 
 function onFormSubmit() {
+    hideForm();
+    const newObj = createTodoObj();
+    clearForm();
+    addTodo(newObj);
+}
+
+function onFormSave() {
+    hideForm();
+    const updatedObj = createTodoObj();
+    clearForm();
+    editTodo(updatedObj);
+}
+
+function displayForm() {
+    submitBtn.textContent = "ADD";
+    addNewBtn.style.display = "none";
+    todoContainer.style.display = "none"
+    todoForm.style.display = "block";
+}
+
+function hideForm() {
+    addNewBtn.style.display = "block";
+    todoContainer.style.display = "block";
+    todoForm.style.display = "none";
+}
+
+function createTodoObj() {
     newTodoObj = {
         "title": todoForm.title.value,
         "price": todoForm.price.value,
         "description": todoForm.description.value,
         "imgUrl": todoForm.imgUrl.value
     }
-    addBtn.style.display = "block";
-    todoContainer.style.display = "block";
-    todoForm.style.display = "none";
-    console.log(newTodoObj);
-    addTodo(newTodoObj);
-}
-
-function addBtnClicked() {
-    addBtn.style.display = "none";
-    todoContainer.style.display = "none"
-    todoForm.style.display = "block";
+    return newTodoObj;
 }
 
 function createNewTodo(todoObj) {
@@ -51,7 +75,17 @@ function createNewTodo(todoObj) {
             case("button"):
                 newItem.textContent = items[2];
                 if (items[2] === "edit") {
-                    newItem.addEventListener("click", e => editTodo(e))
+                    if (todoObj["completed"]) {
+                        newTodo.classList.add("checked-todo")
+                    } else {
+                        newTodo.classList.remove("checked-todo")
+                    }
+                    newItem.addEventListener("click", (e) => {
+                        displayForm();
+                        currentId = e.path[1].id;
+                        populateForm();
+                        submitBtn.textContent = "SAVE";
+                    })
                 } else {
                     newItem.addEventListener("click", e => deleteTodo(e))
                 }
@@ -62,7 +96,7 @@ function createNewTodo(todoObj) {
                 newItem.addEventListener("click", e => itemChecked(e))
                 break;
             case("img"):
-                newItem.src = todoObj[items[2]];
+                newItem.src = validateImg(todoObj[items[2]]);
                 break;
             default:
                 break;
@@ -81,7 +115,7 @@ function getAllTodos() {
                 document.body.style.display = "block";
             });
         })
-        .catch(error => alert(error))
+        .catch(error => alert(`Get All(GET): ${error}`))
 }
 
 function clearAllTodos() {
@@ -94,40 +128,59 @@ function clearAllTodos() {
 function addTodo(todoObj) {
     axios.post(todoUrl, todoObj)
         .then(() => getAllTodos())
-        .catch(error => alert(error))
+        .catch(error => alert(`Add Todo(POST): ${error}`))
 }
 
-function editTodo(event, todoObj) {
-    const todoId = event.path[1].id;
-    axios.put(todoUrl + todoId, todoObj)
-        .then(() => getAllTodos())
-        .catch(error => alert(error))
+function editTodo(todoObj) {
+    axios.put(todoUrl + currentId, todoObj)
+            .then(() => getAllTodos())
+            .catch(error => alert(`Add Todo(PUT): ${error}`))
+}
+
+function populateForm() {
+    url = todoUrl + currentId
+    axios.get(url)
+        .then(response => {
+            todoObj = response.data;
+            submitBtn.textContent = "SAVE";
+            todoForm.title.value = todoObj.title;
+            todoForm.price.value = todoObj.price;
+            todoForm.description.value =todoObj.description;
+            todoForm.imgUrl.value = todoObj.imgUrl;;
+        })
+        .catch(error => alert(`Edit-todo(GET): ${error}`))   
 }
 
 function deleteTodo(event) {
     const todoId = event.path[1].id;
     axios.delete(todoUrl + todoId)
         .then(() => getAllTodos())
-        .catch(error => alert(error))
+        .catch(error => alert(`Delete Todo(DELETE): ${error}`))
 }
-
 
 function itemChecked(event) {
-    event.path[1].style.backgroundColor = "red";
+    const todoId = event.path[1].id;
+    event.path[1].classList.toggle("checked-todo");
+    updatedObj = {
+        "completed": false
+        }
     if (event.target.checked) {
-        updatedObj = {
-            "completed": true
-        }
-    } else {
-         updatedObj = {
-            "completed": false
-        }
-    }
-    editTodo(event, updatedObj)
+        updatedObj.completed = true;
+    } 
+    axios.put(todoUrl + todoId, updatedObj)
+        .then(response => response)
+        .catch(error => alert(`Checked-item(PUT): ${error}`))
 }
 
-function formValidation() {
+function validateImg(imgUrl) {
+    return imgUrl ? imgUrl : "./default.jpeg";
+}
 
+function clearForm() {
+    todoForm.title.value = "";
+    todoForm.price.value = "";
+    todoForm.description.value = "";
+    todoForm.imgUrl.value = "";
 }
 
 
