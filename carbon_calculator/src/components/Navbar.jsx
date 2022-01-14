8d525d75464b50cdac47f0e1e79a2d0490783c56
Dalton Bunker <dbbunker3@gmail.com>
2022-01-14 00:29:27 -0700

    Navlinks added to Navbar

--- carbon_calculator/src/components/Navbar.jsx
+++ carbon_calculator/src/components/Navbar.jsx
@@ -4,7 +4,54 @@ import {NavLink} from 'react-router-dom'
 export default function Navbar() {
     return (
         <div className="NavBar">
-            
+            <NavLink 
+                to="/calculator/electricity" 
+                style={({ isActive }) => {
+                    return {
+                        display: "block",
+                        margin: "1rem 0",
+                        color: isActive ? "red" : ""
+                    };
+                }}
+            >
+                Electricity
+            </NavLink>
+            <NavLink 
+                to="/calculator/flights" 
+                style={({ isActive }) => {
+                    return {
+                        display: "block",
+                        margin: "1rem 0",
+                        color: isActive ? "red" : ""
+                    };
+                    }}
+            >
+                Flights
+            </NavLink>
+            <NavLink 
+                to="/calculator/vehicle" 
+                style={({ isActive }) => {
+                    return {
+                        display: "block",
+                        margin: "1rem 0",
+                        color: isActive ? "red" : ""
+                    };
+                    }}
+            >
+                Vehicle
+            </NavLink>
+            <NavLink 
+                to="/calculator/shipping" 
+                style={({ isActive }) => {
+                    return {
+                        display: "block",
+                        margin: "1rem 0",
+                        color: isActive ? "red" : ""
+                    };
+                }}
+            >
+                Shipping
+            </NavLink>
         </div>
     )
 }
