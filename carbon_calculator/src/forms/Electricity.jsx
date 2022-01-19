7c2ea97df638a93f28d22be64fa5f2dec16b78d7
Dalton Bunker <dbbunker3@gmail.com>
2022-01-18 20:54:27 -0700

    fixed state/province bug and added form validation

--- carbon_calculator/src/forms/Electricity.jsx
+++ carbon_calculator/src/forms/Electricity.jsx
@@ -5,9 +5,9 @@ import './Forms.css'
 export default function Electricity() {
 
     const defaultFormInput = {
-        unit: "",
-        value: "",
-        country: "",
+        unit: "mwh",
+        value: 0,
+        country: "United States",
         state: ""
     }
     const {countries, states, provinces} = require('../locationData.json')
@@ -15,18 +15,42 @@ export default function Electricity() {
 
     function handleInputChange(e) {
         const {name, value} = e.target
-        setFormInput(prevFormInput => {
-            return {
-                ...prevFormInput,
-                [name]: value
-            }
-        })
+        if (value === "Canada") {
+            setFormInput(prevFormInput => {
+                return {
+                    ...prevFormInput,
+                    [name]: value,
+                    state: ""
+                }
+            })
+        } else if (value === "United States") {
+            setFormInput(prevFormInput => {
+                return {
+                    ...prevFormInput,
+                    [name]: value,
+                    state: ""
+                }
+            })
+        } else {
+            setFormInput(prevFormInput => {
+                return {
+                    ...prevFormInput,
+                    [name]: value
+                }
+            })
+        }
     }
 
     function handleSubmit(e) {
         e.preventDefault()
         console.log(formInput)
-        setFormInput(defaultFormInput)
+        if (formInput.value <= 0) {
+            alert("Value must be greater than 0")
+        } else if (["United States", "Canada"].includes(formInput.country) && formInput.state.length === 0) {
+            alert("State is Required")
+        } else {
+            
+        }
     }
 
 
@@ -36,7 +60,6 @@ export default function Electricity() {
                 <div className="input-container">
                     <label>Unit: </label>
                     <select name="unit" onChange={handleInputChange}>
-                        <option value=""></option>
                         <option value="mwh">mwh</option>
                         <option value="kwh">kwh</option>
                     </select>
@@ -48,22 +71,21 @@ export default function Electricity() {
                 <div className="input-container">
                     <label>Country: </label>
                     <select name="country" onChange={handleInputChange}>
-                        <option value=""></option>
                         {Object.keys(countries).map(country => <option key={country} value={country}>{country}</option>)}
                     </select>
                 </div>
                 <div className="input-container" style={{display: formInput.country === "United States" ? "block" : "none"}}>
                     <label>State: </label>
-                    <select name="state" onChange={handleInputChange}>
+                    <select name="state" onChange={handleInputChange} value={formInput.state}>
                         <option value=""></option>
                         {states.map(state => <option key={state} value={state}>{state}</option>)}
                     </select>
                 </div>
                 <div className="input-container" style={{display: formInput.country === "Canada" ? "block" : "none"}}>
                     <label>Provinces: </label>
-                    <select name="state">
+                    <select name="state" onChange={handleInputChange} value={formInput.state}>
                         <option value=""></option>
-                        {provinces.map(province => <option key={province} value="">{province}</option>)}
+                        {provinces.map(province => <option key={province} value={province}>{province}</option>)}
                     </select>
                 </div>
                 <button className="form-submit">Get Estimate</button>
