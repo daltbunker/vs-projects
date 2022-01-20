16e6e5e90fe6dde638a8a37137ff355b1f7a05e8
Dalton Bunker <dbbunker3@gmail.com>
2022-01-20 11:00:33 -0700

    finish flights submit function

--- carbon_calculator/src/forms/Flights.jsx
+++ carbon_calculator/src/forms/Flights.jsx
@@ -1,9 +1,153 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './Forms.css'
+import './Flights.css'
+import axios from 'axios'
 
 export default function Flights() {
+
+    const defaultFormInput = {
+        type: "flight",
+        passengers: 1,
+        legs: [
+          {departure_airport: "", destination_airport: ""},
+        ]
+    }
+    const [formInput, setFormInput] = useState(defaultFormInput)
+    const [searchResults, setSearchResults] = useState([])
+    const [departSearch, setDepartSearch] = useState(true)
+    const {airports} = require('../locationData.json')
+
+    function handleValueChange(e) {
+        const {name, value} = e.target
+        let num = 0
+
+        if (value !== "") {
+            num = parseInt(value)
+        }
+
+        setFormInput(prevFormInput => {
+            return {
+                ...prevFormInput,
+                [name]: num
+            }
+        })
+    }
+
+    function handleAirportChange(e) {
+        let {name, value} = e.target
+        
+        // capitalize value
+        if (value.length > 0) {
+            value = value[0].toUpperCase() + value.slice(1,)
+        }
+        
+        filterSearchResults(value)
+        setFormInput(prevFormInput => {
+            return {
+                ...prevFormInput,
+                legs : [
+                    {
+                        ...prevFormInput.legs[0],
+                        [name]: value
+                    }
+                ]
+            }
+        })
+    }
+
+    function filterSearchResults(value) {
+        if (value.length > 0) {
+            const airportList = Object.keys(airports)
+            const searchParam = new RegExp(value)
+            const results = airportList.filter(airport => searchParam.test(airport))
+            setSearchResults(results)
+        } else {
+            setSearchResults([])
+        }
+    }
+
+    function handleSearchResultSelect(airport, name) {
+        setSearchResults([])
+        setFormInput(prevFormInput => {
+            return {
+                ...prevFormInput,
+                legs : [
+                    {
+                        ...prevFormInput.legs[0],
+                        [name]: airports[airport]
+                    }
+                ]
+            }
+        })
+    }
+
+    function handleAirportInputSelect(isDepart) {
+        setDepartSearch(isDepart)
+        setSearchResults([])
+    }
+
+    function handleSubmit() {
+
+    }
+
+
+
     return (
-        <div>
-            Flights Form
-        </div>
+        <div className="form-container">
+        <form onSubmit={(e) => handleSubmit(e)} autoComplete="off">
+            <div className="input-container">
+                <label>Passengers: </label>
+                <input 
+                    type="number" 
+                    name="passengers" 
+                    value={formInput.passengers} 
+                    onChange={handleValueChange} 
+                />
+            </div>
+            <div className="input-container">
+                <label>Departure: </label>
+                <input 
+                    type="text" 
+                    name="departure_airport" 
+                    value={formInput.legs[0].departure_airport} 
+                    onChange={handleAirportChange} 
+                    onClick={() => handleAirportInputSelect(true)}
+                />
+                <div className="search-results" style={{display: searchResults.length > 0 && departSearch ? "block" : "none"}}>
+                    {searchResults.map(result => 
+                        (  
+                            <div 
+                                key={result} 
+                                className="search-result" 
+                                onClick={() => handleSearchResultSelect(result, "departure_airport")}>{result}
+                            </div>
+                        )
+                    )}
+                </div>
+            </div>
+            <div className="input-container">
+                <label>Destination: </label>
+                <input 
+                    type="text" 
+                    name="destination_airport" 
+                    value={formInput.legs[0].destination_airport} 
+                    onChange={handleAirportChange} 
+                    onClick={() => handleAirportInputSelect(false)}
+                />
+                <div className="search-results" style={{display: searchResults.length > 0 && !departSearch ? "block" : "none"}}>
+                    {searchResults.map(result => 
+                        (  
+                            <div 
+                                key={result} 
+                                className="search-result" 
+                                onClick={() => handleSearchResultSelect(result, "destination_airport")}>{result}
+                            </div>
+                        )
+                    )}
+                </div>
+            </div>
+            <button className="form-submit">Get Estimate</button>
+        </form>
+    </div>
     )
 }
